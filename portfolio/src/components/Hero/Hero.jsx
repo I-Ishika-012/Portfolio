ea26147995699b35003590315cf05570a5767a02
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-17 18:39:01 +0530

    Update Hero.jsx

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -3,13 +3,16 @@ import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
     return (
-        <section>
-            <div>
-                <h1>Hi, I &apos;m Ishika Dutta</h1>
-                <p>Engineering Student and Aspiring Software Developer. To know more...</p>
-                <a href="mailto:i.ishikadutta@gmail.com">Contact Me</a>
+         <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I &apos;m Ishika Dutta</h1>
+                <p className={styles.description}>Engineering Student and Aspiring Software Developer. To know more...</p>
+                <a href="mailto:i.ishikadutta@gmail.com" className={styles.contactBtn}>Contact Me</a>
             </div>
-            <img src={getImageUrl("hero/heroImage.png")} alt="hero" />
+
+            <img src={getImageUrl("hero/heroImage.png")} alt="hero"  className={styles.heroImg}/>
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
         </section>
     );
 }
