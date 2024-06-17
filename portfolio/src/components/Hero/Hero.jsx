6696e4b20dc634d1890d9738453013c0bb33c808
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-17 16:33:44 +0530

    Update Hero.jsx

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import styles from "./Hero.module.css";
 
+export const Hero = () => {
+    return (
+        <div className={styles.Hero}>
+            Hero
+        </div>
+    );
+}
