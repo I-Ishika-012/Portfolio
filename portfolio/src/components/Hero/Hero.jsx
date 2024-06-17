4aca230b5eb2b1e5e7aab31e877d0c67a7d20b5d
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-17 18:31:39 +0530

    Update Hero.jsx

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,10 +1,13 @@
-import React from "react";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
     return (
-        <div className={styles.Hero}>
-            Hero
-        </div>
+        <section>
+            <div>
+                <h1>Hi, I &apos;m Ishika Dutta</h1>
+                <p>Engineering Student and Aspiring Software Developer</p>
+                <a href="mailto:i.ishikadutta@gmail.com">Contact Me</a>
+            </div>
+        </section>
     );
 }
