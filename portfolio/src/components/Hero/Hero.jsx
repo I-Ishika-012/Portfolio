4663679648a213a255a3c67ec0389df647c5d5c0
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-17 18:33:20 +0530

    Update Hero.jsx

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,13 +1,15 @@
 import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
     return (
         <section>
             <div>
                 <h1>Hi, I &apos;m Ishika Dutta</h1>
-                <p>Engineering Student and Aspiring Software Developer</p>
+                <p>Engineering Student and Aspiring Software Developer. To know more...</p>
                 <a href="mailto:i.ishikadutta@gmail.com">Contact Me</a>
             </div>
+            <img src={getImageUrl("hero.png")} alt="hero" />
         </section>
     );
 }
